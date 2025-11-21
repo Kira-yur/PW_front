670d09774293aad6fd24d21f3271d42bd898f223
Kirill Yurin <kirilyur@yandex.ru>
2025-11-21 16:27:26 +0300

    add file with plugin

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright, Page
-import pytest
-
-@pytest.fixture
-def chromium_page() -> Page:
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        yield browser.new_page()
